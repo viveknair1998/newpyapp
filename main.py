98c4552f26eb9516bbcb7f477bdf832be716e6d1
viveknair1998 <78834685+viveknair1998@users.noreply.github.com>
2025-10-20 23:26:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ configure_azure_monitor(
 
   connection_string="InstrumentationKey=8ae92223-6fd7-437f-94a0-fae690b1a688;IngestionEndpoint=https://northeurope-2.in.applicationinsights.azure.com/;LiveEndpoint=https://northeurope.livediagnostics.monitor.azure.com/;ApplicationId=22978c51-b4ad-44e2-bafc-69aaffa10b97",
 
-  enable_live_metrics=True,
+  #enable_live_metrics=True,
 
   logger_name="sunlog",  # Set the namespace for the logger in which you would like to collect telemetry for if you are collecting logging telemetry. This is imperative so you do not collect logging telemetry from the SDK itself.
 
