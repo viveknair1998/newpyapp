285c60b16d01e9fc41e51be707df2aa0c6a269f6
viveknair1998 <78834685+viveknair1998@users.noreply.github.com>
2025-10-20 23:47:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,9 @@
-import logging
-from azure.monitor.opentelemetry import configure_azure_monitor
 from fastapi import FastAPI, Form, Request, status
 from fastapi.responses import HTMLResponse, FileResponse, RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 import uvicorn
 
-# configure_azure_monitor(
-
-#   connection_string="InstrumentationKey=8ae92223-6fd7-437f-94a0-fae690b1a688;IngestionEndpoint=https://northeurope-2.in.applicationinsights.azure.com/;LiveEndpoint=https://northeurope.livediagnostics.monitor.azure.com/;ApplicationId=22978c51-b4ad-44e2-bafc-69aaffa10b97",
-
-#   #enable_live_metrics=True,
-
-#   logger_name="sunlog",  # Set the namespace for the logger in which you would like to collect telemetry for if you are collecting logging telemetry. This is imperative so you do not collect logging telemetry from the SDK itself.
-
-# )
-
-# logger = logging.getLogger("sunlog")  # Logging telemetry will be collected from logging calls made with this logger and all of it's children loggers.
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
@@ -42,6 +29,6 @@ async def hello(request: Request, name: str = Form(...)):
         print('Request for hello page received with no name or blank name -- redirecting')
         return RedirectResponse(request.url_for("index"), status_code=status.HTTP_302_FOUND)
 
-# if __name__ == '__main__':
-#     uvicorn.run('main:app', host='0.0.0.0', port=8000)
+if __name__ == '__main__':
+    uvicorn.run('main:app', host='0.0.0.0', port=8000)
 
