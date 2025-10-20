5d1393e4a2765e7f76163d4c3916fafab2e6f616
viveknair1998 <78834685+viveknair1998@users.noreply.github.com>
2025-10-20 16:50:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,18 @@
 import logging
 from azure.monitor.opentelemetry import configure_azure_monitor
-
-
+import fastapi
 
 configure_azure_monitor(
 
-  #connection_string="InstrumentationKey=8ae92223-6fd7-437f-94a0-fae690b1a688;IngestionEndpoint=https://northeurope-2.in.applicationinsights.azure.com/;LiveEndpoint=https://northeurope.livediagnostics.monitor.azure.com/;ApplicationId=22978c51-b4ad-44e2-bafc-69aaffa10b97",
+  connection_string="InstrumentationKey=8ae92223-6fd7-437f-94a0-fae690b1a688;IngestionEndpoint=https://northeurope-2.in.applicationinsights.azure.com/;LiveEndpoint=https://northeurope.livediagnostics.monitor.azure.com/;ApplicationId=22978c51-b4ad-44e2-bafc-69aaffa10b97",
 
-  #enable_live_metrics=True,
+  enable_live_metrics=True,
 
   logger_name="sunlog",  # Set the namespace for the logger in which you would like to collect telemetry for if you are collecting logging telemetry. This is imperative so you do not collect logging telemetry from the SDK itself.
 
 )
 
 logger = logging.getLogger("sunlog")  # Logging telemetry will be collected from logging calls made with this logger and all of it's children loggers.
-
-
 from fastapi import FastAPI, Form, Request, status
 from fastapi.responses import HTMLResponse, FileResponse, RedirectResponse
 from fastapi.staticfiles import StaticFiles
@@ -23,8 +20,7 @@ from fastapi.templating import Jinja2Templates
 import uvicorn
 
 
-
-app = fastapi.FastAPI(__name__)
+app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
